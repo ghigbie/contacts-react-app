49ad2e7a3503934d46568d75ea0a71ff05001aee
George Higbie <gehigbie@gmail.com>
2018-04-26 18:55:30 +0000

    Add List component

---
+++ src/Components/ListContacts.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class ListComponent extends Component{
+    render(){
+        return(
+            <h1>List component</h1>
+        );
+    }
+}
+
+export default ListComponent;
