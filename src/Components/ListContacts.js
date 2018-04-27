cd21c11d56ccdc8b9ecc7bdf91672120bd91e1d9
George Higbie <gehigbie@gmail.com>
2018-04-27 03:15:53 +0000

    Change format of ListComponents

--- src/Components/ListContacts.js
+++ src/Components/ListContacts.js
@@ -6,8 +6,17 @@ class ListComponent extends Component{
         return(
             <ol className="contact-list">
                 {this.props.contacts.map((contact) => (
-                    <li key={contact.id}>
-                        {contact.name}
+                    <li key={contact.id}
+                        className="contact-list-item">
+                        <div className="contact-avatar"
+                             style={{
+                                 backgroundImage: `url(${contact.avatarURL})`
+                             }}>
+                        </div>
+                        <div className="contact-detail">
+                            <p>{contact.name}</p>
+                            <p>{contact.handle}</p>
+                        </div>
                     </li>
                 ))}s
             </ol>
