fc82d759347ed37ed7a6d0548b363d7da3282b4b
George Higbie <gehigbie@gmail.com>
2018-04-27 13:08:24 +0000

    Add comments

--- src/services/dataService.js
+++ src/services/dataService.js
@@ -3,19 +3,19 @@ const contacts = [
    "id": "karen",
    "name": "Karen Isgrigg",
    "handle": "karen_isgrigg",
-   "avatarURL": "https://react-playground-ghigbie.c9users.io:8081/karen.jpg"
+   "avatarURL": "https://react-playground-ghigbie.c9users.io:8081/karen.jpg" //this data was changed for cloud 9
  },
  {
    "id": "richard",
    "name": "Richard Kalehoff",
    "handle": "richardkalehoff",
-   "avatarURL": "https://react-playground-ghigbie.c9users.io:8081/richard.jpg"
+   "avatarURL": "https://react-playground-ghigbie.c9users.io:8081/richard.jpg" //this data was changed for cloud 9
  },
  {
    "id": "tyler",
    "name": "Tyler McGinnis",
    "handle": "tylermcginnis",
-   "avatarURL": "https://react-playground-ghigbie.c9users.io:8081/tyler.jpg"
+   "avatarURL": "https://react-playground-ghigbie.c9users.io:8081/tyler.jpg" //this data was changed for cloud 9
  }
 ];
 
