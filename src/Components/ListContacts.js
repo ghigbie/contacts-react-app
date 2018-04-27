b8ef9a82dc281efc4a002a33d14c75870f48b70f
George Higbie <gehigbie@gmail.com>
2018-04-27 03:08:35 +0000

    Remove index as key

--- src/Components/ListContacts.js
+++ src/Components/ListContacts.js
@@ -1,10 +1,15 @@
 import React, {Component} from 'react';
 
+
 class ListComponent extends Component{
     render(){
         return(
             <ol className="contact-list">
-                {this.props.contacts.map((contact, index) => (<li key={index}>{contact.name}</li>))}
+                {this.props.contacts.map((contact) => (
+                    <li key={contact.id}>
+                        {contact.name}
+                    </li>
+                ))}s
             </ol>
         );
     }
