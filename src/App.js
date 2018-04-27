f0458e123cb918a6723a44b9b12393302669911c
George Higbie <gehigbie@gmail.com>
2018-04-27 02:56:52 +0000

    Import contacts through app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-
+import contacts from './services/dataService';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello Bubba!
+        <ListContacts contacts={contacts} />
       </div>
     );
   }
--- src/Components/ListContacts.js
+++ src/Components/ListContacts.js
@@ -4,7 +4,7 @@ class ListComponent extends Component{
     render(){
         return(
             <ol className="contact-list">
-            
+                {this.props.contacts.map((contact, index) => (<li key={index}>{contact.name}</li>))}
             </ol>
         );
     }
