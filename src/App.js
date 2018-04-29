bb82fe4f1cd78abc2a754a3e445aedaea9419b8f
George Higbie <gehigbie@gmail.com>
2018-04-29 23:33:28 +0000

    Fix propblem

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
   componentDidMount(){
     ContactsAPI.getAll()
       .then((contacts) => {
-        this.setState(() => ({contacts }));
+        this.setState(() => ({contacts}));
       });
   }
   
--- src/Components/ListContacts.js
+++ src/Components/ListContacts.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import * as ContactsAPI from './utils/ContactsAPI';
 
 class ListContacts extends Component{
     static propTypes = {
