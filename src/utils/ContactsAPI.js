52a7c830a7831e3c5e40409bda6fc0f27abfa331
George Higbie <gehigbie@gmail.com>
2018-04-29 23:37:44 +0000

    Update for c9 server

--- src/utils/ContactsAPI.js
+++ src/utils/ContactsAPI.js
@@ -1,4 +1,4 @@
-const api = process.env.REACT_APP_CONTACTS_API_URL || 'http://localhost:5001'
+const api = process.env.REACT_APP_CONTACTS_API_URL || 'https://react-playground-ghigbie.c9users.io:8081'
 
 let token = localStorage.token
 
