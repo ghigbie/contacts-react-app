3f3c52b188c919709de85677e2f60948db2dc13d
George Higbie <gehigbie@gmail.com>
2018-04-30 16:32:05 +0000

    Add funtion body to handlesubmit function

--- src/Components/CreateContact.js
+++ src/Components/CreateContact.js
@@ -6,6 +6,7 @@ import serializeForm from 'form-serialize';
 class CreateContact extends Component{
     handleSubmit = (e) =>{
         e.preventDefault();
+        const values = serializeForm(e.target, {hash: true});
     }
     
     render(){
