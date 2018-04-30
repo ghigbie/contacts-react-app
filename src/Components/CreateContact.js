42c1b6af46f5217eeb21e5824d97463850dbf6e5
George Higbie <gehigbie@gmail.com>
2018-04-30 15:38:43 +0000

    Add link to homepage

--- src/Components/CreateContact.js
+++ src/Components/CreateContact.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import ImageImports from './ImageImports';
 
 class CreateContact extends Component{
     render(){
         return(
             <div>
-                <h3>Create Contact</h3>
+                <Link className="close-create-contact"
+                      to="/">
+                    Close
+                </Link>
+            
             </div>
         );
     }
