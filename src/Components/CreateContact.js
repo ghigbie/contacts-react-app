50332a8c1ae01f169ec8fb4c9536b9044f168c68
George Higbie <gehigbie@gmail.com>
2018-04-30 00:39:11 +0000

    Add js to create contact component

---
+++ src/Components/CreateContact.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CreateContact extends Component{
+    render(){
+        return(
+            <div>
+                <h3>Create Contact</h3>
+            </div>
+        );
+    }
+}
+
+export default CreateContact;
