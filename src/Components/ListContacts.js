21a19d7e4c2b5abf6b2390ed7167aba96d1d6edb
George Higbie <gehigbie@gmail.com>
2018-04-30 14:39:15 +0000

    Edit onNavigate function call

--- src/Components/ListContacts.js
+++ src/Components/ListContacts.js
@@ -20,7 +20,7 @@ class ListContacts extends Component{
     
     render(){
         const { query } = this.state;
-        const { contacts, onDeleteContact } = this.props;
+        const { contacts, onDeleteContact, onNavigate } = this.props;
         
         const showingContacts = query === '' 
             ? contacts 
@@ -41,7 +41,7 @@ class ListContacts extends Component{
                            value={query}
                            onChange={(event) => this.updateQuery(event.target.value)}/>
                     <a href="#create"
-                       onClick={() => ({})}
+                       onClick={onNavigate}
                        className="add-contact"
                       >Add Contact</a>
                 </div>
